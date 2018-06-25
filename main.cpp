8776cae69e59e396b9d893d9dcac4432496b9482
JacksChoco <ljs8860@playauto.co.kr>
2018-06-25 21:22:32 +0900

    first

---
+++ main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  JongManbook
+//
+//  Created by Jack on 2018. 6. 25..
+//  Copyright © 2018년 Jack. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
